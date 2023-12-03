f3ca5dcd314a6215496d1e59a3ea976e85a53536
shivas1432 <shivashanker7337@gmail.com>
2023-12-03 23:14:00 +0000

    corrected file paths

---
+++ config/config_116.js
@@ -0,0 +1,30 @@
+﻿// Configuration file - Getting better at Node.js
+// End of year, more time to learn
+
+const config116 = {
+    app: {
+        name: 'Learning Project',
+        version: '0.1.0',
+        description: 'Created while learning GitHub and Node.js'
+    },
+    
+    server: {
+        port: process.env.PORT || 3000,
+        host: 'localhost'
+    },
+    
+    database: {
+        // Will add database later when I learn more
+        type: 'none',
+        status: 'learning'
+    },
+    
+    metadata: {
+        created: '2023-12-03',
+        author: 'Beginner developer',
+        id: 116,
+        phase: 'learning GitHub basics'
+    }
+};
+
+module.exports = config116;
