021dc951a9f4188e538fd234684030e5911ae66f
shivas1432 <shivashanker7337@gmail.com>
2023-12-24 03:46:00 +0000

    updated server configuration

---
+++ config/config_748.js
@@ -0,0 +1,30 @@
+﻿// Configuration file - Getting better at Node.js
+// End of year, more time to learn
+
+const config748 = {
+    app: {
+        name: 'Learning Project',
+        version: '0.1.0',
+        description: 'Created while learning GitHub and Node.js'
+    },
+    
+    server: {
+        port: process.env.PORT || 3000,
+        host: 'localhost'
+    },
+    
+    database: {
+        // Will add database later when I learn more
+        type: 'none',
+        status: 'learning'
+    },
+    
+    metadata: {
+        created: '2023-12-24',
+        author: 'Beginner developer',
+        id: 748,
+        phase: 'learning GitHub basics'
+    }
+};
+
+module.exports = config748;
